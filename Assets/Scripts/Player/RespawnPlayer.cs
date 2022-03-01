7f20f366f4c2a409b984c1b19f79c2d79bff8329
ZorroDKK <99809433+ZorroDKK@users.noreply.github.com>
2022-03-01 21:50:04 +0100

    Revert "Merge branch 'master' of https://github.com/JSNPackages/NettoRunner"
    This reverts commit c03c360bc91f8a61d51cc11912fc121d0f87e7dc, reversing
    changes made to 10a0d2b2cb79eed5ebc261b1312ee7c303f261ee.

--- Assets/Scripts/Player/RespawnPlayer.cs
+++ Assets/Scripts/Player/RespawnPlayer.cs
@@ -1,38 +1,37 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class RespawnPlayer : MonoBehaviour {
-    private Vector2 spawnPoint;
-    private Vector2 currentSpawn;
+public class RespawnPlayer : MonoBehaviour
+{
+    Vector2 spawnPos;
     Rigidbody2D rb;
     int health = 3;
 
     // Start is called before the first frame update
     void Start() {
-        this.spawnPoint = transform.position;
-        this.currentSpawn = transform.position;
+        spawnPos = transform.position;
         rb = GetComponent<Rigidbody2D>();
 
     }
 
     public void SetSpawnPoint(Vector2 newSpawn) {
-        this.currentSpawn = newSpawn;
+        spawnPos = newSpawn;
     }
 
     public void RespawnMyPlayer() {
         rb.velocity = Vector2.zero;
+        transform.position = spawnPos;
 
-        if (this.health == 0) {
-            this.currentSpawn = this.spawnPoint;
-        }
-        
-        transform.position = this.currentSpawn.normalized;
     }
-    
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Enemy")) {
-            this.health -= 1;
-            RespawnMyPlayer();
+            health -= 1;
+        }
+    }
+    private void Update() {
+        if (health == 0) {
+            print("u dead man?");
         }
     }
 }
